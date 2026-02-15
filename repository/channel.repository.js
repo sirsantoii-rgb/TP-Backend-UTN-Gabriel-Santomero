ede96f6dcf022b27ff7147ab19fe289df79d9664
GabS.Dev <el_santofr@hotmail.com>
2026-02-15 18:15:41 +0100

    46

--- repository/channel.repository.js
+++ repository/channel.repository.js
@@ -12,6 +12,23 @@ class ChannelRepository {
     async getByIdAndWorkspaceId(channel_id, workspace_id){
         return await Channel.findOne({_id: channel_id, fk_id_workspace: workspace_id})
     }
+
+    // NUEVA: Para renombrar el canal
+    async update(channel_id, workspace_id, name){
+        return await Channel.findOneAndUpdate(
+            { _id: channel_id, fk_id_workspace: workspace_id }, // Filtro de seguridad
+            { name: name },
+            { new: true } // Para que devuelva el canal ya modificado
+        )
+    }
+
+    // NUEVA: Para eliminar el canal
+    async delete(channel_id, workspace_id){
+        return await Channel.findOneAndDelete({
+            _id: channel_id, 
+            fk_id_workspace: workspace_id 
+        })
+    }
 }
 
 const channelRepository = new ChannelRepository()
