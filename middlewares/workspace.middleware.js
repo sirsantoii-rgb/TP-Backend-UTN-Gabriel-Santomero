707cc36af1ffe708b683a8bf69f16f8ffe58db00
GabS.Dev <el_santofr@hotmail.com>
2026-02-15 16:57:24 +0100

    39

--- middlewares/workspace.middleware.js
+++ middlewares/workspace.middleware.js
@@ -1,11 +1,6 @@
-/* 
-Verificar que el workspace exista
-Verificar que el usuario sea miembro del workspace
-Verificar que tenga el rol correcto
-*/
-
 import ServerError from "../helpers/error.helpers.js"
 import workspaceRepository from "../repository/workspace.repository.js"
+import mongoose from "mongoose"
 
 function workspaceMiddleware(authorized_roles = []) {
 
@@ -15,31 +10,49 @@ function workspaceMiddleware(authorized_roles = []) {
             const user_id = request.user.id
             const workspace_id = request.params.workspace_id
 
+            console.log("Middleware start")
+
+            // 1. VALIDAR OBJECT ID
+            if (!mongoose.Types.ObjectId.isValid(workspace_id)) {
+                throw new ServerError("ID de workspace inválido", 400)
+            }
+
+            console.log("ID válido")
+
+            // 2. BUSCAR WORKSPACE
             const workspace_selected = await workspaceRepository.getById(workspace_id)
+            console.log("Workspace buscado")
 
             if (!workspace_selected) {
                 throw new ServerError('No existe ese espacio de trabajo', 404)
             }
 
-            //Obtenemos la membresia
-            const member_selected = await workspaceRepository.getMemberByWorkspaceIdAndUserId(workspace_id, user_id)
+            // 3. BUSCAR MEMBRESÍA
+            const member_selected = await workspaceRepository.getMemberByWorkspaceIdAndUserId(
+                workspace_id,
+                user_id
+            )
+            console.log("Membresía buscada")
 
             if (!member_selected) {
                 throw new ServerError("No perteneces a este espacio de trabajo", 403)
             }
 
-            //Gestionar acceso por role de miembro
+            // 4. VALIDAR ROL
             if (authorized_roles.length > 0 && !authorized_roles.includes(member_selected.role)) {
                 throw new ServerError("No estas autorizado para hacer esta operacion", 403)
             }
 
+            // 5. SET REQUEST
             request.workspace = workspace_selected
             request.member = member_selected
+
+            console.log("Middleware OK")
             next()
         }
         catch (error) {
             console.log("Error en workspace.middleware", error)
-            /* Si tiene status decimos que es un error controlado (osea es esperable) */
+
             if (error.status) {
                 return response.json({
                     status: error.status,
